fa3d0719f1dd458649c1778291e640fe07ed518a
Camelia Ciuca <camelia1414@gmail.com>
2025-02-24 11:26:18 +0100

    changes in game.py, created functions move_player and main, the game is working

--- src/game.py
+++ src/game.py
@@ -1,7 +1,8 @@
 from src.grid import Grid
 from src.player import Player
 from src.pickups import pickups, randomize, Item
-from src.score import print_status, update_score
+from src.score import Score
+
 
 def move_player(player, dx, dy, grid, score):
     """Försöker flytta spelaren och uppdatera poäng om det behövs."""
@@ -14,35 +15,42 @@ def move_player(player, dx, dy, grid, score):
         player.move(dx, dy)
 
         if isinstance(maybe_item, Item):
-            score = update_score(maybe_item.value, score)
+            score.update_score(maybe_item.value)
             print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
             grid.clear(new_x, new_y)
 
-    return score
 
-player = Player(2, 1)
-inventory = []
-score = 0
-grid = Grid()
-grid.set_player(player)
-grid.make_walls()
-randomize(grid)
+def main():
+    """Huvudfunktion för spelet."""
+    player = Player(2, 1)
+    inventory = []
+    score = Score()
+    grid = Grid()
+    grid.set_player(player)
+    grid.make_walls()
+    randomize(grid)
 
-command = "a"
-# Loopa tills användaren trycker Q eller X.
-while not command.casefold() in ["q", "x"]:
-    print_status(grid, score)
+    command = "a"
+    # Loopa tills användaren trycker Q eller X.
+    while not command.casefold() in ["q", "x"]:
+        print("--------------------------------------")
+        print(f"You have {score.get_score()} points.")
+        print(grid)
 
-    command = input("Use WASD to move, Q/X to quit. ")
-    command = command.casefold()[:1]
+        command = input("Use WASD to move, Q/X to quit. ")
+        command = command.casefold()[:1]
 
-    moves = {"w": (0, -1),
-             "a": (-1, 0),
-             "s": (0, 1),
-             "d": (1, 0)}
-    if command in moves:
-        dx, dy = moves[command]
-        score = move_player(player, dx, dy, grid, score)
+        moves = {"w": (0, -1),
+                 "a": (-1, 0),
+                 "s": (0, 1),
+                 "d": (1, 0)}
+        if command in moves:
+            dx, dy = moves[command]
+            move_player(player, dx, dy, grid, score)
 
-# Hit kommer vi när while-loopen slutar
-print(f"Thank you for playing!Your final score is: {score}")
+    print(f"Thank you for playing! Your final score is: {score.get_score()}")
+
+
+# Kör spelet endast om filen körs direkt
+if __name__ == "__main__":
+    main()
